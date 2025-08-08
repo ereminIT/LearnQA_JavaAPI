f2f2e1235c54c332b9fc075237f356c6ae18be50
meremin <m.eremin@korona.net>
2025-08-08 19:57:14 +0700

    Ex12: Тест запроса на метод header

--- src/test/java/Homework3.java
+++ src/test/java/Homework3.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -18,4 +20,18 @@ public class Homework3 {
         //System.out.println(cookies);
         assertTrue(cookies.containsKey("HomeWork"),"Response doesn't have 'HomeWork' cookie");
     }
+
+    //Ex12: Тест запроса на метод header
+    @Test
+    public void HomeworkHeader(){
+        Response responseGetHeader = RestAssured
+                .given()
+                .post("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        Headers headers = responseGetHeader.getHeaders();
+        //System.out.println(headers);
+        assertTrue(headers.hasHeaderWithName("Content-Type"), "Response doesn't have 'Content-Type' cookie");
+    }
+
+
 }
